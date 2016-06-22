30f38cd6a32ed78f720ecce56395ae83ca423bc3
iD Student <iDStudent@ids-imac-84.local>
2016-06-22 10:18:31 -0700

    Initial Commit

---
+++ Day3SpacevaderTests/Day3SpacevaderTests.swift
@@ -0,0 +1,36 @@
+//
+//  Day3SpacevaderTests.swift
+//  Day3SpacevaderTests
+//
+//  Created by iD Student on 6/22/16.
+//  Copyright © 2016 iD Student. All rights reserved.
+//
+
+import XCTest
+@testable import Day3Spacevader
+
+class Day3SpacevaderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
